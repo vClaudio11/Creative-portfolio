5b605cb67b6e340fc0a6702b962467cbeebab7e5
vClaudio11 <vonclaudio.college@gmail.com>
2026-07-08 10:30:31 +0800

    feat: add footer copyright

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
     return(
         <div className="left-0 bottom-0 w-full h-16 bg-accent">
             <div className="flex flex-row w-full h-full justify-between items-center px-8 md:px-12">
-                <p className="text-secondary-foreground">Von Claudio</p>
+                <p className="text-secondary-foreground">© 2026 Von Claudio</p>
                 <nav>
                     <Link href="#hero" className="hover:underline">Home</Link>
                 </nav>
